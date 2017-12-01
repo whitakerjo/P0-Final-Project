a2c928bf906ad29cbc0419bca33d7bb80977aaf4
whitakerjo <whitakerjo@berea.edu>
2017-12-01 09:06:51 -0500

    Created a new file with class Nim as a seperate file to import from. Committing this file.

--- P0.py
+++ P0.py
@@ -1,1 +1,16 @@
-P0
+from Nim import Nim
+import turtle
+
+class Ball:
+
+    def __init__(self):
+        self.size = 0
+        self.color = ""
+
+    def draw_balls(self, answer):
+        for balls in answer:
+
+
+
+
+
