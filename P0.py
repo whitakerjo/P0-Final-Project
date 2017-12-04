13c7fe7806dfac91482bd5517abe7fabccba94bf
whitakerjo <whitakerjo@berea.edu>
2017-12-04 09:00:33 -0500

    I now have a method of where the program asks the user for amount of balls and then draws that many on the turtle screen.

--- P0.py
+++ P0.py
@@ -7,8 +7,70 @@ class Ball:
         self.size = 0
         self.color = ""
 
-    def draw_balls(self, answer):
-        for balls in answer:
+    def draw_balls(self, balls):
+        cody = turtle.Turtle()
+        cody.penup()
+        cody.goto(-300, 0)
+        cody.pendown()
+        if balls == 15:
+            for balls in range(15):
+                cody.circle(25)
+                cody.penup()
+                cody.forward(60)
+                cody.pendown()
+        elif balls == 16:
+            for balls in range(16):
+                cody.circle(25)
+                cody.penup()
+                cody.forward(60)
+                cody.pendown()
+        elif balls == 17:
+            for balls in range(17):
+                cody.circle(25)
+                cody.penup()
+                cody.forward(60)
+                cody.pendown()
+        elif balls == 18:
+            for balls in range(18):
+                cody.circle(25)
+                cody.penup()
+                cody.forward(60)
+                cody.pendown()
+        elif balls == 19:
+            for balls in range(19):
+                cody.circle(25)
+                cody.penup()
+                cody.forward(60)
+                cody.pendown()
+        elif balls == 20:
+            for balls in range(20):
+                cody.circle(25)
+                cody.penup()
+                cody.forward(60)
+                cody.pendown()
+        else:
+            print("Invalid answer. Please pick another.")
+
+    def user_picks(self):
+        '''
+        This function is where the user will click on the screen with the amount of balls they want to take for their turn.
+        :return:
+        '''
+
+
+
+def main():
+    wn = turtle.Screen()
+    wn.screensize(1200, 760, "blue")
+
+    ball = Ball()
+    balls = int(input("Please pick a number of balls to choose from between 15 and 20."))
+    ball.draw_balls(balls)
+
+    wn.exitonclick()
+
+
+main()
 
 
 
