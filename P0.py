2f5a8b1c07df3b4b387699ca3dd4e8c946d6a4e8
whitakerjo <whitakerjo@berea.edu>
2017-12-11 09:35:30 -0500

    Another Daily Commit.

--- P0.py
+++ P0.py
@@ -3,12 +3,12 @@ from nim_class import *
 
 
 def main():
-    n = Nim()
-    n.introduce_user()
-    n.create_balls()
+    n = Nim()                                                                       #nim object to call all nim methods
+    n.introduce_user()                                                              #introduces the user and asks how many balls to play with
+    n.create_balls()                                                                #creates all the balls given the amount they choose
     n.user_turn()
-    # n.computer_turn()
-    n.wn.onkey(n.end_turn(), "Space")
+    # n.comput er_turn()
+    n.wn.onkey(n.end_turn, "space")
     n.wn.listen()
     n.wn.mainloop()
 
